66bed42f7c736951e68f180f50ea5cf954650969
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-10-24 17:53:16 -0200

    Redirecionando rota / para clientes/

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@ require_once '../bootstrap.php';
 $gerador = new \JRP\Gerador\Gerador();
 $clientes = $gerador->clientes();
 
+$app->get('/', function() use ($app)
+{
+    return $app->redirect('/clientes');
+});
+
 $app->get('/clientes', function() use($clientes)
 {
     return new \Symfony\Component\HttpFoundation\JsonResponse($clientes);

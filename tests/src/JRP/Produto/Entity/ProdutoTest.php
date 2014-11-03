5d1c249f7d1d75de0a02e3c8464ac9bdee4b9633
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-11-03 00:09:52 -0200

    Criando ProdutoTest.php

--- tests/src/JRP/Produto/Entity/ProdutoTest.php
+++ tests/src/JRP/Produto/Entity/ProdutoTest.php
@@ -7,7 +7,61 @@ class ProdutoTest extends \PHPUnit_Framework_TestCase {
 
     public function testVerificaEncapsulamentoSetId()
     {
+        $produto = new Produto();
+        $produto->setId(1);
 
+        $this->assertEquals(1, $produto->getId());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSeMetodoSetIdRetornaExcecaoAoSetarValorNaoInteiro()
+    {
+        $produto = new Produto();
+        $produto->setId('id');
+    }
+
+    public function testVerificaEncapsulamentoSetValor()
+    {
+        $produto = new Produto();
+        $produto->setValor(100);
+
+        $this->assertEquals(100, $produto->getValor());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testVerificaSeMetodoSetValorRetornaExcecaoAoSetarValorNaoNumerico()
+    {
+        $produto = new Produto();
+        $produto->setValor('valor');
+    }
+
+    public function testVerificaEncapsulamentoSetNome()
+    {
+        $produto = new Produto();
+        $produto->setNome('nome');
+
+        $this->assertEquals('nome', $produto->getNome());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testVerificaSeMetodoSetNomeRetornaExcecaoAoSetarNomeVazio()
+    {
+        $produto = new Produto();
+        $produto->setNome('');
+    }
+
+    public function testVerificaEncapsulamentoSetDescricao()
+    {
+        $produto = new Produto();
+        $produto->setDescricao('descrição');
+
+        $this->assertEquals('descrição', $produto->getDescricao());
     }
 
 }

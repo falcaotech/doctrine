9828a34cbb38ce66b44f7ae72bb73cecbd00452c
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-11-03 00:47:17 -0200

    Testando mappers

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,24 @@
 
 require_once 'vendor/autoload.php';
 
+use JRP\Db\Db;
+use Aura\Di\Container;
+use Aura\Di\Factory;
+
+$di = new Container(new Factory());
+$db = new Db("mysql:host=localhost;dbname=soa", "root", "82214290");
+
+$di->set('db.conn', $db->getConn());
+
 $app = new Silex\Application(array('debug' => true));
+
+$produtoMapper = new \JRP\Produto\Mapper\ProdutoMapper($di);
+
+$produto = new \JRP\Produto\Entity\Produto();
+$produto->setId(1);
+$produto->setNome('teste');
+$produto->setValor(100);
+
+var_dump($produtoMapper->update($produto));
+
+var_dump($produtoMapper->delete($produto));
--- web/index.php
+++ web/index.php
@@ -2,17 +2,16 @@
 
 require_once '../bootstrap.php';
 
-$gerador = new \JRP\Gerador\Gerador();
-$clientes = $gerador->clientes();
-
 $app->get('/', function() use ($app)
 {
-    return $app->redirect('/clientes');
 });
 
 $app->get('/clientes', function() use($clientes)
 {
+    $gerador = new \JRP\Gerador\Gerador();
+    $clientes = $gerador->clientes();
+
     return new \Symfony\Component\HttpFoundation\JsonResponse($clientes);
 });
 
-$app->run();
+//$app->run();

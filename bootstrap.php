ccbcfb74c793dfbd6f08d3b8a19d4536698d3c0d
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-11-09 22:28:57 -0200

    Adicionando Twig

--- bootstrap.php
+++ bootstrap.php
@@ -13,6 +13,10 @@ $di->set('db.conn', $db->getConn());
 
 $app = new \JRP\Application\App(array('debug' => true));
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app['produtoService'] = function() use ($di){
     $produtoMapper = new \JRP\Produto\Mapper\ProdutoMapper($di);
     $produtoService = new \JRP\Produto\Service\ProdutoService($produtoMapper, new \JRP\Produto\Entity\Produto());

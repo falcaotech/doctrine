ddb0fbe29d149df9192630f643546aa260f66f68
Walter Araújo Gomes Júnior <walter.jrp@live.com>
2014-11-10 01:17:24 -0200

    Adicionando view para cadastro de produtos

--- web/index.php
+++ web/index.php
@@ -4,20 +4,32 @@ require_once '../bootstrap.php';
 
 $app->get('/', function() use ($app)
 {
-    return $app->redirect('/produto/listagem');
+    return $app->redirect($app['url_generator']->generate('produtos'));
 });
 
-$app->get('/produto/listagem', function() use ($app)
+$app->get('/produtos', function() use ($app)
 {
     $produtos = $app['produtoService']->read();
 
     return $app['twig']->render('produtos/listagem.twig', ['produtos' => $produtos]);
 })->bind('produtos');
 
-$app->get('/produto/cadastrar', function()
+$app->get('/produto/cadastrar', function() use($app)
 {
-
+    return $app['twig']->render('produtos/cadastrar.twig');
 })->bind('produto-cadastrar');
+
+$app->post('/produto/cadastrar', function(\Symfony\Component\HttpFoundation\Request $request) use ($app)
+{
+    $dados = $request->request->all();
+
+    if($app['produtoService']->insert($dados))
+    {
+        return $app->redirect($app['url_generator']->generate('produtos'));
+    }
+
+    return "Erro ao cadastrar produto!";
+});
 
 $app->get('/produto/editar/{id}', function($id) use ($app)
 {
